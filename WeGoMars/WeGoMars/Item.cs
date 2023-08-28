042a0f6f2c419b12f5a12211dc6e1a820047fafb
pjayas <115007528+pjayas@users.noreply.github.com>
2023-08-28 16:00:22 +0900

    Create Item.cs
    아이템 클래스(박종수)

---
+++ WeGoMars/WeGoMars/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WeGoMars
+{
+    public class Item
+    {
+        public string Name { get; }
+        public string Code { get; }
+        public string ItemType { get; }
+        public string Info { get; }
+        public int Atk { get; }
+        public int Def { get; }
+        public int Hp { get; }
+        public int Price { get; }
+
+        public Item(string name, string code, string itemType, string info, int atk, int def, int hp, int price)
+        {
+            Name = name;
+            Code = code;
+            ItemType = itemType;
+            Info = info;
+            Atk = atk;
+            Def = def;
+            Hp = hp;
+            Price = price;
+        }
+    }
+}
+
