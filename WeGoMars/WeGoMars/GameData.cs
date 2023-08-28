f47d59bf3bab345515c64399c558862cf5557351
장성림 <hoobhoob214@gmail.com>
2023-08-28 18:49:05 +0900

    [ADD] Json 패키지 설치 및 GameData.cs 완성 (-성림)
    [ADD]
    WeGoMars/WeGoMars/
    WeGoMars.csproj 에 Json패키지 설치
    GameData.cs
    (-성림)

--- WeGoMars/WeGoMars/GameData.cs
+++ WeGoMars/WeGoMars/GameData.cs
@@ -1,4 +1,7 @@
 
+using Newtonsoft.Json;
+using System.Numerics;
+
 namespace WeGoMars
 {
     public class GameData
@@ -6,14 +9,73 @@ namespace WeGoMars
         private List<Player> players;
         private List<Monster> monsters;
         private List<Item> itemList;
+        private string filePath;
 
-        public Item GetItemFromCode(string code);
 
-        public Player GetPlayer(string name);
+        public GameData(string filePath)
+        {
+            this.filePath = filePath;
+            
+            if (File.Exists(filePath + "PlayerData.Json"))
+            {
+                string readItemString = File.ReadAllText(filePath + "ItemData.Json");
+                itemList = JsonConvert.DeserializeObject<List<Item>>(readItemString);
+            }
+            else
+                this.itemList = new List<Item>();
 
-        public Player GetPlayer(int num);
+            if (File.Exists(filePath + "PlayerData.Json"))
+            {
+                string readMonsterString = File.ReadAllText(filePath + "MonsterData.Json");
+                monsters = JsonConvert.DeserializeObject<List<Monster>>(readMonsterString);
+            }
+            else
+                this.monsters = new List<Monster>();
 
-        public Monster GetPlayer(int num);
+            if (File.Exists(filePath + "PlayerData.Json"))
+            {
+                string readPlayerString = File.ReadAllText(filePath + "PlayerData.Json");
+                players = JsonConvert.DeserializeObject<List<Player>>(readPlayerString);
+            }
+            else
+                this.players = new List<Player>();
+        }
+
+        public Item GetItemFromCode(string code)
+        {
+            foreach (Item item in itemList)
+            {
+                if (item.Code == code)
+                    return item;
+            }
+            return null;
+        }
+
+        public Player GetPlayer(string name)
+        {
+            foreach (Player player in players)
+            {
+                if (player.Name == name)
+                    return player;
+            }
+            return null;
+        }
+
+        public Player GetPlayer(int num)
+        {
+            if (num >= 0 && num < players.Count)
+                return players[num];
+            else
+                return null;
+        }
+
+        public Monster GetMonster(int num)
+        {
+            if (num >= 0 && num < monsters.Count)
+                return monsters[num];
+            else
+                return null;
+        }
 
     }
 }
