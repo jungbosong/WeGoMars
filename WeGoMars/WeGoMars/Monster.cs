46b26b7409cb2545c16ce0ef9c2fb3867349437f
장성림 <hoobhoob214@gmail.com>
2023-08-28 20:25:18 +0900

    [UPDATE] 몬스터 MaxHp/Mp 추가 (-성림)

--- WeGoMars/WeGoMars/Monster.cs
+++ WeGoMars/WeGoMars/Monster.cs
@@ -3,13 +3,15 @@ namespace WeGoMars
 {
     public class Monster : Character
     {
-        public Monster(string name, string job, int level, int atk, int def, int hp, int mp, int gold, int exp, List<Skill> skillList, List<Item> inventory)
+        public Monster(string name, string job, int level, int atk, int def, int maxHp, int maxMp, int hp, int mp, int gold, int exp, List<Skill> skillList, List<Item> inventory)
         {
             Name = name;
             Job = job;
             Level = level;
             Atk = atk;
             Def = def;
+            MaxHp = maxHp;
+            MaxMp = maxMp;
             Hp = hp;
             Mp = mp;
             Gold = gold;
