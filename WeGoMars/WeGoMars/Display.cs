693f7c63755964f45b9d6ccb3abb6c0b7a601c4e
bosomi <she79560@naver.com>
2023-08-28 15:37:23 +0900

    [ADD] 화면 UI구성에 필요한 클래스 추가 (-지연)

---
+++ WeGoMars/WeGoMars/Display.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeGoMars
+{
+    internal abstract class Display
+    {
+        public void SetTitle(string title)
+        {
+
+        }
+        public void SetAction(string actions)
+        {
+
+        }
+        public void WriteWrongInput()
+        {
+
+        }
+
+    }
+}
---
+++ WeGoMars/WeGoMars/DungeonScene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeGoMars
+{
+    internal class DungeonScene
+    {
+        public void DisplayDungeon()
+        {
+
+        }
+        public void DisplayPlayerTurn(string player)
+        {
+
+        }
+        public void DisplayMonsterTurn(string monster)
+        {
+
+        }
+        public void DisplayResult()
+        {
+
+        }
+
+    }
+}
