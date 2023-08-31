51b432cced5a63b0ac621b7131124c176ccc7296
bosomi <she79560@naver.com>
2023-08-31 20:03:55 +0900

    [ADD] 인벤토리 정렬기능 추가 (-지연)

--- WeGoMars/WeGoMars/InventoryScene.cs
+++ WeGoMars/WeGoMars/InventoryScene.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Numerics;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace WeGoMars
 {
     internal class InventoryScene : Displayer
     {
         List<string> itemList = new List<string>();
+        
         public void DisplayInventory()
         {
             SetTitle($"{MsgDefine.INVENTORY}\n");
@@ -15,9 +17,8 @@ namespace WeGoMars
             SetItemList();
             WriteItemList(0, 4, itemList);
 
-            SetAction($"1. {MsgDefine.MANAGE_EQUIP}0. {MsgDefine.OUT}");
-            //2. {MsgDefine.SORT_ITEM}
-            int input = CheckValidInput(0, 1);
+            SetAction($"1. {MsgDefine.MANAGE_EQUIP}2. {MsgDefine.SORT_ITEM}0. {MsgDefine.OUT}");
+            int input = CheckValidInput(0, 2);
             switch (input)
             {
                 case 0:
@@ -27,7 +28,7 @@ namespace WeGoMars
                     DisplayManageEquipment();
                     break;
                 case 2:
-                    //DisplaySortItem();
+                    DisplaySortItem();
                     break;
             }
         }
@@ -53,6 +54,41 @@ namespace WeGoMars
             }
         }
 
+        public void DisplaySortItem()
+        {
+            SetTitle($"{MsgDefine.INVENTORY} - {MsgDefine.SORT_ITEM}");
+            Console.Write(MsgDefine.EXPLAN_INVENTORY);
+            Console.WriteLine();
+
+            SetItemList();
+            WriteItemList(0, 10, itemList);
+
+            SetAction($"1. {MsgDefine.NAME}2. {MsgDefine.EQUIPPED}3. {MsgDefine.OFFENSIVE_POWER}\n4. {MsgDefine.DEFENSIVE_POWER}\n0. {MsgDefine.OUT}");
+            int input = CheckValidInput(0, 4);
+            switch (input)
+            {
+                case 0:
+                    DisplayInventory();
+                    break;
+                case 1:
+                    SortItemList(MsgDefine.NAME);
+                    DisplaySortItem();
+                    break;
+                case 2:
+                    SortItemList(MsgDefine.EQUIPPED);
+                    DisplaySortItem();
+                    break;
+                case 3:
+                    SortItemList(MsgDefine.OFFENSIVE_POWER);
+                    DisplaySortItem();
+                    break;
+                case 4:
+                    SortItemList(MsgDefine.DEFENSIVE_POWER);
+                    DisplaySortItem();
+                    break;
+            }
+        }
+
         public void SetItemList()
         {
             itemList.Clear();
@@ -67,15 +103,33 @@ namespace WeGoMars
                 }
                 if (item.Type == ItemType.Armor)
                 {
-                    tmp += string.Format("{0,-15}|{1,-10} +{2}|{3,-30}\n", item.Name, MsgDefine.DEFENSIVE_POWER, item.Def, item.Info);
+                    tmp += $"{item.Name}|{MsgDefine.DEFENSIVE_POWER} +{item.Def}|{item.Info}\n";
                 }
                 else
                 {
-                    tmp += string.Format("{0,-15}|{1,-10} +{2}|{3,-30}\n", item.Name, MsgDefine.OFFENSIVE_POWER, item.Atk, item.Info);
+                    tmp += $"{item.Name}|{MsgDefine.OFFENSIVE_POWER} +{item.Atk}|{item.Info}\n";
                 }
-
                 itemList.Add(tmp);
             }
         }
+
+        public void SortItemList(string sortBy)
+        {
+            switch (sortBy)
+            {
+                case MsgDefine.NAME:
+                    Managers.Player.Inventory = Managers.Player.Inventory.OrderByDescending(item => item.Name.Replace(" ", string.Empty).Length).ToList();
+                    break;
+                case MsgDefine.EQUIPPED:
+                    Managers.Player.Inventory = Managers.Player.Inventory.OrderByDescending(item => item.Equipped).ToList();
+                    break;
+                case MsgDefine.OFFENSIVE_POWER:
+                    Managers.Player.Inventory = Managers.Player.Inventory.OrderByDescending(item => item.Atk).ToList();
+                    break;
+                case MsgDefine.DEFENSIVE_POWER:
+                    Managers.Player.Inventory = Managers.Player.Inventory.OrderByDescending(item => item.Def).ToList();
+                    break;
+            }
+        }
     }
- }
+}
